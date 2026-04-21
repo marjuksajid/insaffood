fb45f573448555ebbc7e8e4f8bc7c73b70986e3a
marjuksajid <marjuksajid@protonmail.com>
2026-04-22 05:53:59 +0600

    Update database configuration to use environment variable for DATABASE_URL

--- project/settings.py
+++ project/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
